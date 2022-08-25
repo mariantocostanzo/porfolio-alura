2460bf6d653f483c126925d80c30845ae7922554
negucostanzo@hotmail.com <negucostanzo@hotamil.com>
2022-08-24 23:04:50 -0300

    porfolio para alura

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+import { habilitarBotonEnviar } from "./formulario/habilitarBoton.js";
+import menu from "../js/componentes/menu.js";
+import validarCampos from "./formulario/validarCampos.js";
+
+(() => {
+    /**
+     * @const
+     * Contiene el elemento form del DOM.
+     */
+    const formularioContacto = document.querySelector(".formulario-container__formulario");
+
+    menu();
+    validarCampos();
+    habilitarBotonEnviar();
+
+    /**
+     * Usando delegación de eventos para inputs y textarea.
+     * Cuando el usuario escriba en los campos de tipo input
+     * o textArea se ejecutará la función habilitarBotonEnviar
+     * para comprobar si se desbloqueará el botón.
+     */
+    const validarBtnFormulario = (event) => {
+        const element = event.target;
+        if (element && element.tagName == 'INPUT') {
+            habilitarBotonEnviar();
+        } else if (element && element.tagName == 'TEXTAREA') {
+            habilitarBotonEnviar();
+        }
+    }
+
+    formularioContacto.addEventListener("keyup", validarBtnFormulario);
+})();
